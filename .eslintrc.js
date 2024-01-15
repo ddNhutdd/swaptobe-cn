c5bd3363a9f775acdf0322e16b952522148f4beb
CN <dgxa9999@gmail.com>
2024-01-15 13:16:08 +0700

    ff

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parserOptions: { sourceType: "module" },
-  parser: "babel-eslint",
-  plugins: ["react-hooks"],
-  rules: {
-    "react-hooks/exhaustive-deps": "off",
-  },
-};
